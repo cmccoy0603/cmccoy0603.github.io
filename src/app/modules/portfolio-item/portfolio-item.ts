c7fecfadcdcec3fae590dc427d79f95042bf1b8c
cmccoy0603 <cvmccoy123@gmail.com>
2025-08-19 18:00:46 -0500

    Added pages and bootstrap

---
+++ src/app/modules/portfolio-item/portfolio-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-item',
+  imports: [],
+  templateUrl: './portfolio-item.html',
+  styleUrl: './portfolio-item.scss'
+})
+export class PortfolioItem {
+
+}
